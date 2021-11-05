30c759eb2a1b6f8713bc55bf8c1d8edf318821c4
mark <616421019@qq.com>
2021-11-05 09:26:15 +0800

    parse ![[md#^block id]]

--- src/mindmap/INode.ts
+++ src/mindmap/INode.ts
@@ -1,5 +1,5 @@
 import MindMap from './mindmap' 
-import {MarkdownRenderer,normalizePath,TFile} from 'obsidian'
+import {MarkdownRenderer,normalizePath,TFile,parseLinktext,resolveSubpath} from 'obsidian'
 import {t} from '../lang/helpers'
 
 
@@ -117,52 +117,115 @@ export default class Node {
     }
 
     _delay(){
-         //parse image
-         this.contentEl.findAll(".internal-embed").forEach((el) => {
+           //parse md
+           this.contentEl.findAll(".internal-embed").forEach(async (el) => {
             const src = el.getAttribute("src");
-            const target =
-              typeof src === "string" &&
-              this.mindmap&&this.mindmap.view?.app.metadataCache.getFirstLinkpathDest(src, this.mindmap.path);
-            if (target instanceof TFile && target.extension !== "md" && this.mindmap) {
-              el.innerText = "";
-              el.createEl(
-                "img",
-                { attr: { src: this.mindmap.view.app.vault.getResourcePath(target) } },
-                (img) => {
-                  if (el.hasAttribute("width"))
-                    img.setAttribute("width", el.getAttribute("width"));
-                  if (el.hasAttribute("alt"))
-                    img.setAttribute("alt", el.getAttribute("alt"));
-                }
-              );
-              el.addClasses(["image-embed", "is-loaded"]);
+            if(typeof src ==='string'){
+                var pathObj=parseLinktext(src);
+                var fileData ='';
+               if(this.mindmap&&this.mindmap.view){
+                    var f = this.mindmap.view.app.metadataCache.getFirstLinkpathDest(pathObj.path,this.mindmap.path);
+                    if(f instanceof TFile&&f.extension ==='md'){
+                         fileData = await this.mindmap.view.app.vault.adapter.read(f.path);
+                         var markdownEmbed = document.createElement('div');
+                         markdownEmbed.classList.add('markdown-embed');
+                        //  var  markdownHead = document.createElement('div');
+                        //  markdownHead.classList.add('markdown-embed-title');
+                        //  markdownHead.innerText=f.basename;
+                         markdownEmbed.setAttribute('data-name',f.path);
+                         var markdownContent = document.createElement('div');
+                         markdownContent.classList.add('markdown-embed-content');
+                         var markdownPreview = document.createElement('div');
+                         markdownPreview.classList.add('markdown-preview-view');
+                         markdownContent.appendChild(markdownPreview);
+                         var markdownLink = document.createElement('div');
+                         markdownLink.classList.add('markdown-embed-link');
+                         markdownLink.setAttribute('aria-label','Open link');
+                         markdownLink.innerHTML = `<a data-href="${src}" href="${src}" class="internal-link" target="_blank" rel="noopener"><svg viewBox="0 0 100 100" class="link" width="20" height="20"><path fill="currentColor" stroke="currentColor" d="M74,8c-4.8,0-9.3,1.9-12.7,5.3l-10,10c-2.9,2.9-4.7,6.6-5.1,10.6C46,34.6,46,35.3,46,36c0,2.7,0.6,5.4,1.8,7.8l3.1-3.1 C50.3,39.2,50,37.6,50,36c0-3.7,1.5-7.3,4.1-9.9l10-10c2.6-2.6,6.2-4.1,9.9-4.1s7.3,1.5,9.9,4.1c2.6,2.6,4.1,6.2,4.1,9.9 s-1.5,7.3-4.1,9.9l-10,10C71.3,48.5,67.7,50,64,50c-1.6,0-3.2-0.3-4.7-0.8l-3.1,3.1c2.4,1.1,5,1.8,7.8,1.8c4.8,0,9.3-1.9,12.7-5.3 l10-10C90.1,35.3,92,30.8,92,26s-1.9-9.3-5.3-12.7C83.3,9.9,78.8,8,74,8L74,8z M62,36c-0.5,0-1,0.2-1.4,0.6l-24,24 c-0.5,0.5-0.7,1.2-0.6,1.9c0.2,0.7,0.7,1.2,1.4,1.4c0.7,0.2,1.4,0,1.9-0.6l24-24c0.6-0.6,0.8-1.5,0.4-2.2C63.5,36.4,62.8,36,62,36 z M36,46c-4.8,0-9.3,1.9-12.7,5.3l-10,10c-3.1,3.1-5,7.2-5.2,11.6c0,0.4,0,0.8,0,1.2c0,4.8,1.9,9.3,5.3,12.7 C16.7,90.1,21.2,92,26,92s9.3-1.9,12.7-5.3l10-10C52.1,73.3,54,68.8,54,64c0-2.7-0.6-5.4-1.8-7.8l-3.1,3.1 c0.5,1.5,0.8,3.1,0.8,4.7c0,3.7-1.5,7.3-4.1,9.9l-10,10C33.3,86.5,29.7,88,26,88s-7.3-1.5-9.9-4.1S12,77.7,12,74 c0-3.7,1.5-7.3,4.1-9.9l10-10c2.6-2.6,6.2-4.1,9.9-4.1c1.6,0,3.2,0.3,4.7,0.8l3.1-3.1C41.4,46.6,38.7,46,36,46L36,46z"></path></svg></a>`
+                       
+                         el.appendChild(markdownEmbed);
+                        //  markdownEmbed.appendChild(markdownHead);
+                         markdownEmbed.appendChild(markdownContent);
+                         markdownEmbed.appendChild(markdownLink);
+
+                        if(pathObj.subpath){
+                            var metacache = this.mindmap.view.app.metadataCache.getFileCache(f);
+                            var t=resolveSubpath(metacache,pathObj.subpath);
+                         //   console.log(t);
+                            if(t&&t.start&&t.end){
+                              var md =fileData.substring(t.start.offset,t.end.offset);
+                             // console.log(md)
+                            }else if(t&&t.start&&!t.end){
+                                var md = fileData.substr(t.start.offset);
+                            }else{
+                                var md = fileData||'';
+                            }
+                        }else{
+                            var md=fileData||'';
+                        }
+
+                        if(md){
+                            MarkdownRenderer.renderMarkdown(md,markdownPreview,this.mindmap.path||"",null).then(()=>{
+                               // this.data.mdText = this.editDom.innerHTML;
+                                this.refreshBox();
+                                //this._delay();
+                                this.mindmap&&this.mindmap.emit('renderEditNode',{node:this});
+                            });
+                        }
+
+                    }
+               }
             }
           });
-          
-        //Possible causes of delay,code mathjax  
-        var dom =this.contentEl.querySelector('code')|| this.contentEl.querySelector('.MathJax');
-        if(dom){
-            setTimeout(()=>{
-                this.clearCacheData();
-                this.refreshBox();
-                this.mindmap&&this.mindmap.emit('renderEditNode',{});
-            },100);
-        }
-        //image 
-        this.contentEl.querySelectorAll('img').forEach(element => {
-            element.onload = () => {
+         //parse image
+         setTimeout(()=>{
+             this.contentEl.findAll(".internal-embed").forEach((el) => {
+                const src = el.getAttribute("src");
+                const target =
+                  typeof src === "string" &&
+                  this.mindmap&&this.mindmap.view?.app.metadataCache.getFirstLinkpathDest(src, this.mindmap.path);
+                if (target instanceof TFile && target.extension !== "md" && this.mindmap) {
+                  el.innerText = "";
+                  el.createEl(
+                    "img",
+                    { attr: { src: this.mindmap.view.app.vault.getResourcePath(target) } },
+                    (img) => {
+                      if (el.hasAttribute("width"))
+                        img.setAttribute("width", el.getAttribute("width"));
+                      if (el.hasAttribute("alt"))
+                        img.setAttribute("alt", el.getAttribute("alt"));
+                    }
+                  );
+                  el.addClasses(["image-embed", "is-loaded"]);
+                }
+              });
+              
+            //Possible causes of delay,code mathjax  
+            var dom =this.contentEl.querySelector('code')|| this.contentEl.querySelector('.MathJax');
+            if(dom){
+                setTimeout(()=>{
                     this.clearCacheData();
                     this.refreshBox();
                     this.mindmap&&this.mindmap.emit('renderEditNode',{});
+                },100);
             }
-            element.onerror = () => {
-                    this.clearCacheData();
-                    this.refreshBox();
-                    this.mindmap&&this.mindmap.emit('renderEditNode',{});
-            }
+            //image 
+            this.contentEl.querySelectorAll('img').forEach(element => {
+                element.onload = () => {
+                        this.clearCacheData();
+                        this.refreshBox();
+                        this.mindmap&&this.mindmap.emit('renderEditNode',{});
+                }
+                element.onerror = () => {
+                        this.clearCacheData();
+                        this.refreshBox();
+                        this.mindmap&&this.mindmap.emit('renderEditNode',{});
+                }
+    
+                element.setAttribute('draggble','false');
+            });
 
-            element.setAttribute('draggble','false');
-        });
+         },100)
     }
 
     select(){
@@ -188,9 +251,10 @@ export default class Node {
     edit(){
         this.contentEl.innerText='';
         this._oldText = this.data.text;
-        var _t =  this.data.text.replace(/\r\n/g,"<br/>")
-        _t = _t.replace(/\n/g,"<br/>");
-        this.contentEl.innerHTML = _t;
+        //var _t =  this.data.text.replace(/\r\n/g,"<br/>")
+       // _t = _t.replace(/\n/g,"<br/>");
+      //  console.log(_t);
+        this.contentEl.innerText = this.data.text;
         this.contentEl.setAttribute('contentEditable','true');
         this.contentEl.focus();
         this.mindmap.editNode = this;
